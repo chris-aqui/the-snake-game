badbe7b549be874e1bdf1802343de5dc3d687f64
Christine Aqui <2739361+christine-aqui@users.noreply.github.com>
2018-09-14 14:45:11 -0400

    fun code along

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coding Challenge #3: The Snake Game
+
+p5.js version of the snake game
+
+
+## Notes:
+p5.js requires a setup and draw function.
---
+++ sketch.js
@@ -0,0 +1,59 @@
+
+console.log("loaded sketch");
+let s; // snake
+let scl = 20;
+
+let food;
+
+function setup () {
+  console.log("setup canvus");
+  // define the canvas size.
+  createCanvas(600, 600);
+
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+
+}
+
+function pickLocation () {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed () {
+  s.total++;
+}
+
+function draw () {
+  console.log('drawing canvus')
+  //  background color
+  background(51);
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death();
+  s.update();
+  s.show();
+
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+
+function keyPressed () {
+  // control the snake direction
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1); //(x , y)
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1); //(x , y)
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0); //(x , y)
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0); //(x , y)
+  }
+}
